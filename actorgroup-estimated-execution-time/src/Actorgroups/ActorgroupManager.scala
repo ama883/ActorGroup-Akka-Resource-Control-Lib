16c01f46d2ebae27caf2440ae7ccd6a26fecca7a
ama883 <ama883@mail.usask.ca>
2016-10-02 12:27:44 -0600

    Add files via upload

---
+++ actorgroup-estimated-execution-time/src/Actorgroups/ActorgroupManager.scala
@@ -0,0 +1,203 @@
+////////////////////////////////////////////////////////////////////////////////
+//  Description :   This file defines the ActorgroupManager class.     
+//                  The ActorGroup Manager is responsible for book-keeping about ticks consumed by managed actors as a result of delivery of messages.  A new actorgroup interested in receiving resources registers itself with the ActorGroup Manager. Once the registration request is received, the ActorGroup Manager instantly adds the actorgroup to its resource scheduling, and the actorgroup begins receiving ticks from the next interval on.  Although the current implementation does not have the ability to reserve resources not beginning immediately, a new request can be made at runtime for an actorgroup, overriding previous allocations.
+//  Author      :   Ahmed Abdel Moamen (ama883@mail.usask.ca)
+//  Date        :   September 27th, 2016
+//  Version     :   1.0   
+////////////////////////////////////////////////////////////////////////////////
+
+//
+//  package.
+//
+package Actorgroups
+
+//
+//  import
+//
+
+import scala.concurrent.duration.Duration
+import java.util.concurrent.TimeUnit
+import java.util.ArrayList
+import akka.actor.ActorRef
+import scala.collection.JavaConverters._
+import akka.actor.Actor
+import akka.actor.Props
+import akka.actor.ActorSystem
+import scala.concurrent.duration.FiniteDuration
+import java.io.File
+import com.typesafe.config.ConfigFactory
+
+
+object ActorgroupManagerObject
+{
+  
+  var m_instance: ActorgroupManager = null;
+ 
+  /**
+     *  returns a singelton of ActorgroupManager
+     */
+   def getInstance(): ActorgroupManager= {
+      if (m_instance == null) {
+         m_instance = new ActorgroupManager();
+         // load the config file
+         val myConfigFile = new File("src/resources/actorgroup.conf")
+         val fileConfig = ConfigFactory.parseFile(myConfigFile).getConfig("actorgroup")
+         val config = ConfigFactory.load(fileConfig)
+         // load the interval time
+          m_instance.m_interval = config.getLong("interval") 
+    }
+    return m_instance;
+  }
+   
+  
+ class ActorgroupManager {
+ 
+// 
+//  parameters
+//
+   var m_lActorgroups: ArrayList[Actorgroup] = null // list of Actorgroups in this ActorSystem
+   var m_interval : Long = 1 // the time interval (rate) for all Actorgroups in the system
+   var m_intervalUnit: TimeUnit = TimeUnit.SECONDS // the unit of resources in this Actorgroup
+   
+  
+   /**
+     *  returns the interval
+     */
+   def getInterval(): Long =
+   {
+     return m_interval;
+   }
+   
+   /**
+     *  returns the interval unit
+     */
+   def getIntervalUnit(): TimeUnit =
+   {
+     return m_intervalUnit;
+   }
+   
+    /**
+     *  register a Actorgroup
+     * @param p_Actorgroup: The actorgroup which is added to the current Actorgroup list
+     */
+   def registerActorgroup(p_Actorgroup: Actorgroup)
+   {
+      if (m_lActorgroups == null)
+      {
+        m_lActorgroups = new ArrayList[Actorgroup]()
+      }
+      m_lActorgroups.add(p_Actorgroup)
+   }
+   
+    /** 
+     * Checks if an Actor has more ticks for execution
+     * Called by the dispatcher before scheduling an actor for execution    
+     * Returns true if the actor has can be scheduled 
+     * @param p_anActor: The name of the actor to be scheduled
+     */
+   def isSchedullable(p_Actor: ActorRef): Boolean=
+   {
+//       println("ActorgroupManager-isSchedullable: " + p_Actor.toString())
+     if(m_lActorgroups ne null)
+     {
+       for (cyber <- m_lActorgroups.asScala) {
+         // if the Actorgroup which belongs to the actor is active, it returns true
+            if(cyber.isActorFound(p_Actor))  
+            {
+              return cyber.isActive()
+            }
+       }
+     }
+     // return true if actor is not found to be belonging to any Actorgroup
+      return true 
+   }
+   
+   
+    /** 
+     * Check if an Actor has more ticks for execution
+     * Called by the dispatcher before scheduling an actor for execution    
+     * Returns true if the actor has can be scheduled 
+     * @param p_anActor: The name of the actor to be scheduled
+     * @param executionTime: the amout of resources assigned to the actor
+     */
+   def isSchedullable(p_Actor: ActorRef, executionTime: Long): Boolean=
+   {
+     if(m_lActorgroups ne null)
+     {
+       for (cyber <- m_lActorgroups.asScala) {
+         // if the Actorgroup which belongs to the actor is active, it returns true
+            if(cyber.isActorFound(p_Actor))  
+            {
+              if(cyber.getTickCounter() >= executionTime) 
+              {
+                reportExecutionTime(p_Actor, executionTime)
+                return true
+              }
+              else
+              {
+                return false
+              }
+            }
+       }
+     }
+     // return true if actor is not found to be belonging to any Actorgroup
+      return true 
+   }
+   
+   
+   /** 
+     * Check if an Actor belongs to a Actorgroup
+     * Returns true if the actor is found
+     * @param p_anActor: The name of the actor to be searched
+     */
+    def isActorFound(p_Actor: ActorRef): Boolean ={ 
+     if(m_lActorgroups ne null)
+     {
+       for (cyber <- m_lActorgroups.asScala) {
+         // if the Actorgroup which belongs to the actor is active, it returns true
+            if(cyber.isActorFound(p_Actor))  
+            {
+                return true
+            }
+       }
+     }
+     // return false if actor not found to be belonging to any Actorgroup
+      return false
+    }
+   
+  /**
+    * Update the number ticks of a Actorgroup  
+    * Called by the dispatcher in order to report an execution time by an actor belongs to this Actorgroup
+    * @p_time in nanosecnds
+  */
+   def reportExecutionTime(p_Actor: ActorRef, p_time: Long)
+   {
+     if(m_lActorgroups ne null)
+     {
+       for (cyber <- m_lActorgroups.asScala) {
+            if(cyber.isActorFound(p_Actor))  
+            {
+              cyber.consumeTicks(p_time) // report execution time
+            }
+       }
+     }
+   }
+   
+   /**
+    * reset the execution time of all Actorgroups in the actor system
+  */
+   def resetExecutionTime()
+   {
+     if(m_lActorgroups ne null)
+     {
+       for (cyber <- m_lActorgroups.asScala) {
+         cyber.consumeInterval()  
+         cyber.resetTicks()
+       }
+     }
+   }
+   
+  
+ }
+ 
+}
